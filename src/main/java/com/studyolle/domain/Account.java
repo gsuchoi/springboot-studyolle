229889adcaed32b1e3064467b8566e37e7b764bc
gsu0925 <gsu925@naver.com>
2020-12-07 14:50:27 +0900

    Add Account domain class

---
+++ src/main/java/com/studyolle/domain/Account.java
@@ -0,0 +1,57 @@
+package com.studyolle.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id") //id만 사용하는 이유는 모르겠음
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Account {
+    // * annotation @Entity를 사용할때는 pk가 있어야 함.
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified; // 이메일 인증을 위한 유무.
+
+    private String emailCheckToken; // 이메일 검증을 위한 토큰값.
+
+    private LocalDateTime joinDt; // 인증된 회원의 가입날짜.
+
+    //기본 프로필
+
+    private String bio;
+
+    private String url;
+
+    private String occupation; // 직업
+
+    private String location;  // varchar(255)
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImg;
+
+    // 알림 설정 관련
+    private boolean studyCreatedByEmail;  // 스터디가 만들어졌다는 걸 받을 방법.
+
+    private boolean studyCreatedByWeb;
+
+    private boolean studyEnrollmentResultByEmail;  // 스터디가 운영하는 모임에 가입신청 결과를 받는 방법.
+
+    private boolean studyEnrollmentResultByWeb;
+
+    private boolean studyUpdatedByEmail;  // 스터디에 대한 변경 사항을 받는 방법.
+
+    private boolean studyUpdatedByWeb;
+
+}
